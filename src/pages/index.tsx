6071fc008e878b15fc11041edff951635a3f9cec
YuriiRG <yuriirg@tutanota.com>
2023-01-06 20:15:14 +0100

    Install TailwindCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <>Hello, World!</>;
+  return (
+    <article className='prose m-4 mt-10 max-w-prose'>
+      <h1>Hello, World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos rem
+        consectetur maxime voluptas nostrum, libero perferendis sapiente ipsam
+        laboriosam enim labore nobis distinctio porro, voluptates repellat
+        similique dolores, dolore deleniti.
+      </p>
+    </article>
+  );
 }
