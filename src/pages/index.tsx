928b4ed4fde000de196e0d76b0cd78d5d9db2f4a
YuriiRG <yuriirg@tutanota.com>
2023-01-07 14:37:19 +0100

    Add dummy interaction with db to check hosting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import { Dummy } from '@prisma/client';
+import { GetServerSideProps } from 'next';
+import { prisma } from '../server/prisma';
+
+type HomeProps = {
+  dummies: Dummy[];
+};
+
+export default function Home({ dummies }: HomeProps) {
   return (
     <article className='prose m-4 mt-10 max-w-prose'>
       <h1>Hello, World!</h1>
@@ -8,6 +16,26 @@ export default function Home() {
         laboriosam enim labore nobis distinctio porro, voluptates repellat
         similique dolores, dolore deleniti.
       </p>
+      <ul>
+        {dummies.map((dummy) => (
+          <li key={dummy.id}>
+            id: {dummy.id}; data: {dummy.data}
+          </li>
+        ))}
+      </ul>
     </article>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  await prisma.dummy.create({
+    data: {
+      data: context.req.headers['user-agent']
+    }
+  });
+
+  const dummies = await prisma.dummy.findMany();
+
+  const props: HomeProps = { dummies };
+  return { props };
+};
