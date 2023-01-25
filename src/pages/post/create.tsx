47350c9c5c4b23eac225fd8e088238048947ab22
YuriiRG <yuriirg@tutanota.com>
2023-01-25 20:32:51 +0100

    Create very simple and ugly create post page

---
+++ src/pages/post/create.tsx
@@ -0,0 +1,31 @@
+import Router from 'next/router';
+import { useState } from 'react';
+import Layout from '../../components/Layout';
+import { api } from '../../utils/api';
+
+export default function CreatePost() {
+  const { mutate, isLoading } = api.post.create.useMutation({
+    onSuccess: (data) => {
+      Router.push(`/post/${data}`);
+    }
+  });
+  const [content, setContent] = useState('');
+
+  return (
+    <Layout className='flex justify-center'>
+      <div className='flex flex-col'>
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <button
+          onClick={() => {
+            mutate({ content, title: content.split('\n').at(0) ?? 'Untitled' });
+          }}
+        >
+          {isLoading ? 'Loading...' : 'Publish'}
+        </button>
+      </div>
+    </Layout>
+  );
+}
