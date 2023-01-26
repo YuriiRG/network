9edfe9cb1af16deb45a68ac44dab92a341209fcf
YuriiRG <yuriirg@tutanota.com>
2023-01-26 17:34:57 +0100

    Install TipTap text editor

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -1,6 +1,7 @@
 import Router from 'next/router';
 import { useState } from 'react';
 import Layout from '../../components/Layout';
+import PostEditor from '../../features/editor/PostEditor';
 import { api } from '../../utils/api';
 
 export default function CreatePost() {
@@ -15,11 +16,8 @@ export default function CreatePost() {
 
   return (
     <Layout className='flex justify-center'>
-      <div className='flex flex-col'>
-        <textarea
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        ></textarea>
+      <div className='flex w-prose flex-col'>
+        <PostEditor />
         <button
           onClick={() => {
             mutate({ content, title: content.split('\n').at(0) ?? 'Untitled' });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      spacing: {
+        prose: '65ch'
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography')]
 };
