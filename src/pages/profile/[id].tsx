a2a3f42ad3509e306f5ffd108f730b112086f237
YuriiRG <yuriirg@tutanota.com>
2023-03-11 19:00:35 +0100

    Create simple, unstyled, read-only profile page

---
+++ src/pages/profile/[id].tsx
@@ -0,0 +1,43 @@
+import type { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { z } from 'zod';
+import Layout from '../../components/Layout';
+import { createSSRHelpers } from '../../server/helpers/ssr';
+import { api } from '../../utils/api';
+
+export default function Profile() {
+  const router = useRouter();
+  const { id: idSlug } = router.query;
+  const id = z.string().uuid().safeParse(idSlug).success
+    ? z.string().parse(idSlug)
+    : 'error';
+
+  const { data } = api.user.get.useQuery(
+    { id },
+    {
+      enabled: id !== 'error'
+    }
+  );
+  if (id === 'error') {
+    return <Layout>Invalid addr</Layout>;
+  }
+  if (data === undefined) {
+    return <Layout>Loading...</Layout>;
+  }
+  if (data === null) {
+    return <Layout>Post doesn&apos;t exist</Layout>;
+  }
+  return <Layout>{data.name}</Layout>;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const ssr = await createSSRHelpers(context);
+  if (z.string().uuid().safeParse(context.params?.id).success) {
+    await ssr.user.get.prefetch({ id: z.string().parse(context.params?.id) });
+  }
+  return {
+    props: {
+      trpcState: ssr.dehydrate()
+    }
+  };
+}
--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,7 +1,15 @@
+import { z } from 'zod';
 import { router, procedure } from '../trpc';
 
 export const userRouter = router({
   all: procedure.query(async ({ ctx }) => {
     return await ctx.prisma.user.findMany();
+  }),
+  get: procedure.input(z.object({ id: z.string() })).query(async ({ ctx, input }) => {
+    return await ctx.prisma.user.findUnique({
+      where: {
+        id: input.id
+      }
+    });
   })
 });
